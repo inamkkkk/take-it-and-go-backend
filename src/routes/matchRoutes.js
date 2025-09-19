b2e84df2881cd340ceb3a888dc2ed0c7fe75a12d
inamkkkk <inamk9025@gmail.com>
2025-09-20 02:10:56 +0500

    fix: refine src/routes/matchRoutes.js

--- src/routes/matchRoutes.js
+++ src/routes/matchRoutes.js
@@ -29,8 +29,22 @@ const findMatchesSchema = {
       type: Joi.string().optional(),
     }).required(),
     // Optional: preferredDeliveryDate, maxBudget, etc.
+    preferredDeliveryDate: Joi.date().optional(),
+    maxBudget: Joi.number().min(0).optional(),
   }),
 };
+
+// TODO: Add route for fetching a specific match by ID
+router.get('/:id', authenticate, authorize(['shipper', 'driver']), matchController.getMatchById);
+
+// TODO: Add route for accepting/rejecting a match (for drivers)
+router.put('/:id/status', authenticate, authorize(['driver']), matchController.updateMatchStatus);
+
+// TODO: Add route for getting a list of matches for a shipper
+router.get('/shipper', authenticate, authorize(['shipper']), matchController.getShipperMatches);
+
+// TODO: Add route for getting a list of matches for a driver
+router.get('/driver', authenticate, authorize(['driver']), matchController.getDriverMatches);
 
 router.post('/find', authenticate, authorize(['shipper']), validate(findMatchesSchema), matchController.findMatches);
 
